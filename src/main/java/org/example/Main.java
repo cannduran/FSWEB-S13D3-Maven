4fbf2518214fe88cff4a9f031d3c6aa3c2bcbd56
Can Duran <canahmetduran0@gmail.com>
2025-04-14 18:32:20 +0300

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,29 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        Person person = new Person("John", "Doe", 20);
+        System.out.println("Firstname: " + person.getFirstName());
+        System.out.println("LastName: " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
+
+        Person secondPerson = new Person("Can", "Duran", 25);
+        System.out.println("Firstname: " + secondPerson.getFirstName());
+        System.out.println("LastName: " + secondPerson.getLastName());
+        System.out.println("Age: " + secondPerson.getAge());
+
+        System.out.println("------------------------------------");
+        Wall wall = new Wall(5, 4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
+
     }
 }
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    int height;
+    int weight;
+    String education;
+    public Person (String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person (String firstName, String lastName, int age, int height, int weight, String education){
+        this(firstName, lastName, age);
+        this.height = height;
+        this.weight = weight;
+        this.education = education;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public boolean isTeen (){
+        if(this.age >= 13 && this.age <= 19){
+            return true;
+        }
+        return false;
+    }
+
+}
